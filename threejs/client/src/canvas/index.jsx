1bd61c04099144001ddfa6523222d35e70a1339e
Peter Fabi <kehindetemitayo.b@gmail.com>
2023-10-03 11:35:12 +0100

    shirt done and tabs added

--- threejs/client/src/canvas/index.jsx
+++ threejs/client/src/canvas/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
+import Shirt from './Shirt';
+import CameraRig from './CameraRig';
+import Backdrop from './Backdrop';
+import { useSnapshot } from 'valtio';
+import state from '../store';
 
-const Canvas = () => {
+const CanvasModel = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Canvas</div>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov:25 /*fov: snap.intro ? 10 :  25*/ }}
+      gl={{preserveDrawingBuffer: true}}
+      className='w-full max-w-full h-full transiton-all ease-in'
+    >
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+
+       <CameraRig>
+          <Backdrop/> 
+          <Center>
+            <Shirt/>
+          </Center>
+       </CameraRig> 
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
--- threejs/client/src/pages/Customizer.jsx
+++ threejs/client/src/pages/Customizer.jsx
@@ -12,21 +12,51 @@ import {AIPicker, ColorPicker, Tab, FilePicker, CustomButton} from '../component
 
 
 const Customizer = () => {
-  const snap = useSnapshot(state)
+  const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  //set the active tab  
+
+  //show tab content depending on the active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />
+      case 'filepicker':
+        return <FilePicker />
+      case 'aipicker':
+        return <AIPicker />
+      default:
+       return null;
+    }
+  }
+
+
   return (
     <AnimatePresence>
       {!snap.intro && (
         <>
           <motion.div key='custom' className='absolute top-0 left-0 z-10' {...slideAnimation('left')}>
             <div className='flex items-center min-h-screen'>
-              <div className='editortabs-container tab'>
+              <div className='editortabs-container tabs'>
                 {EditorTabs.map((tab) => (
                   <Tab
                     key={tab.name}
                     tab={tab}
-                    handleClick={() => {}}
+                    handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div> 
@@ -38,8 +68,16 @@ const Customizer = () => {
                 customStyles='w-fit px-4 py-2.5 font-bold text-sm'
               />
           </motion.div>
-          <motion.div className='filtertabs-container' {}>
-
+          <motion.div className='filtertabs-container' {...slideAnimation('up')}>
+          {FilterTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    isFilterTab
+                    isActiveTab=''
+                    handleClick={() => {}}
+                  />
+                ))}
           </motion.div>
         </>
       )}

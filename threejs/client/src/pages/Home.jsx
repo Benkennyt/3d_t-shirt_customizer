4151d01ca7cde421ffd50ed5ac016ae660ac2c81
Peter Fabi <kehindetemitayo.b@gmail.com>
2023-12-16 19:12:52 +0100

    fourth commit

--- threejs/client/src/pages/Home.jsx
+++ threejs/client/src/pages/Home.jsx
@@ -12,25 +12,21 @@ const Home = () => {
       {snap.intro && (
         <motion.section className='home' {...slideAnimation('left')}>
           <motion.header {...slideAnimation('down')}>
-            <img
-              src='./threejs.png'
-              alt='logo'
-              className='w-8 h-8 object-contain'
-            />
+            <h1 className='logo'>Shirtzerr</h1>
           </motion.header>
           <motion.div className='home-content' {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
-              <h1 className='head-text'>
-                LET'S <br className='xl:block hidden'/> DO IT
-              </h1>
+              <h2 className='intro-header head-text'>
+                Make it <br className='xl:block hidden'/> HAPPEN
+              </h2>
             </motion.div>
-            <motion.div {...headContainerAnimation} className='flex flex-col gap-5'>
+            <motion.div {...headContainerAnimation} className='flex flex-col gap-1'>
               <p className='max-w-md font-normal text-gray-600 text-base'>
                  Create your unique and exclusive shirt with our brand-new 3D customization tool.  <strong>Unleash your imagination</strong> and define your own style
               </p>
               <CustomButton
                 type='filled'
-                title='Customize It'
+                title='Customize'
                 handleClick={() => state.intro = false}
                 customStyle='w-fit px-4 py-2.5 font-bold text-sm'
               />
